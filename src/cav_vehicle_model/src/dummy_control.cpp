7452c09846bc285dc4d06e957b3339c354b15cc1
rghahremani <rghahremani@stu.okan.edu.tr>
2021-05-30 22:07:39 +0300

    updating vehicle model and dummy controller

--- src/cav_vehicle_model/src/dummy_control.cpp
+++ src/cav_vehicle_model/src/dummy_control.cpp
@@ -4,8 +4,8 @@
 #include <cmath>
 
 float x_1, x_2, x_3, x_4, x_5, x_6;
-float T_1, T_2, T_3, T_4, T_5, T_6, T_1_U, T_1_W, T_2_U, T_2_W, 
-T_3_U, T_3_W, T_4_U, T_4_W, T_5_U, T_5_W, INITIAL_U, INITIAL_W, FINAL_U, FINAL_W;
+float T_1, T_2, T_3, T_4, T_5, T_6, T_7, T_1_U, T_1_W, T_2_U, T_2_W, 
+T_3_U, T_3_W, T_4_U, T_4_W, T_5_U, T_5_W, T_6_U, T_6_W, INITIAL_U, INITIAL_W, FINAL_U, FINAL_W;
 //float dx_1, dx_2, dx_3, dx_4, dx_5, dx_6;
 bool feedback_msg_received = false;
 
@@ -38,27 +38,30 @@ int main(int argc, char **argv)
     n.param<float>("dt", dt, 0.00000025);
     
     ros::Publisher output_pub = n.advertise<cav_vehicle_model_msgs::VehicleModelInput>("input", 1000);
-    ros::Subscriber vehicle_feedback_sub = n.subscribe("ouput", 1000, feedBackCallback);
-    n.param<float>("T_1", T_1, 0.01);           
-    n.param<float>("T_2", T_2, 0.01);           
-    n.param<float>("T_3", T_3, 0.01);           
-    n.param<float>("T_4", T_4, 0.01);           
-    n.param<float>("T_5", T_5, 0.01);           
-    n.param<float>("T_6", T_6, 0.01);           
-    n.param<float>("T_1_U", T_1_U, 0.01);           
-    n.param<float>("T_1_W", T_1_W, 0.01);           
-    n.param<float>("T_2_U", T_2_U, 0.01);           
-    n.param<float>("T_2_W", T_2_W, 0.01); 
-    n.param<float>("T_3_U", T_3_U, 0.01);           
-    n.param<float>("T_3_W", T_3_W, 0.01);
-    n.param<float>("T_4_U", T_4_U, 0.01);           
-    n.param<float>("T_4_W", T_4_W, 0.01);
-    n.param<float>("T_5_U", T_5_U, 0.01);           
-    n.param<float>("T_5_W", T_5_W, 0.01);
-    n.param<float>("INITIAL_U", INITIAL_U, 0.01);           
-    n.param<float>("INITIAL_W", INITIAL_W, 0.01);           
-    n.param<float>("FINAL_U", FINAL_U, 0.01);           
-    n.param<float>("FINAL_W", FINAL_W, 0.01);           
+    // ros::Subscriber vehicle_feedback_sub = n.subscribe("ouput", 1000, feedBackCallback);
+    n.param<float>("dummy_control/T_1", T_1, 0.01);           
+    n.param<float>("dummy_control/T_2", T_2, 0.01);           
+    n.param<float>("dummy_control/T_3", T_3, 0.01);           
+    n.param<float>("dummy_control/T_4", T_4, 0.01);           
+    n.param<float>("dummy_control/T_5", T_5, 0.01);           
+    n.param<float>("dummy_control/T_6", T_6, 0.01);           
+    n.param<float>("dummy_control/T_7", T_6, 0.01);           
+    n.param<float>("dummy_control/T_1_U", T_1_U, 0.01);           
+    n.param<float>("dummy_control/T_1_W", T_1_W, 0.01);           
+    n.param<float>("dummy_control/T_2_U", T_2_U, 0.01);           
+    n.param<float>("dummy_control/T_2_W", T_2_W, 0.01); 
+    n.param<float>("dummy_control/T_3_U", T_3_U, 0.01);           
+    n.param<float>("dummy_control/T_3_W", T_3_W, 0.01);
+    n.param<float>("dummy_control/T_4_U", T_4_U, 0.01);           
+    n.param<float>("dummy_control/T_4_W", T_4_W, 0.01);
+    n.param<float>("dummy_control/T_5_U", T_5_U, 0.01);           
+    n.param<float>("dummy_control/T_5_W", T_5_W, 0.01);
+    n.param<float>("dummy_control/T_6_U", T_5_U, 0.01);           
+    n.param<float>("dummy_control/T_6_W", T_5_W, 0.01);
+    n.param<float>("dummy_control/INITIAL_U", INITIAL_U, 0.01);           
+    n.param<float>("dummy_control/INITIAL_W", INITIAL_W, 0.01);           
+    n.param<float>("dummy_control/FINAL_U", FINAL_U, 0.01);           
+    n.param<float>("dummy_control/FINAL_W", FINAL_W, 0.01);           
 
     ros::Rate loop_rate(1000);
     ros::Time start_time = ros::Time::now();
@@ -78,23 +81,35 @@ int main(int argc, char **argv)
         if (dur.toSec() < T_1) {
             vehicle_input.vehicle_control_signals.u = INITIAL_U;
             vehicle_input.vehicle_control_signals.w = INITIAL_W;
+            // std::cout << "-------------TIME DIFF 1: " << dur.toSec() << std::endl;
 
-        } else if (dur.toSec() > T_1 && dur.toSec() < T_2) {
+        } else if (dur.toSec() < T_2) {
+            // std::cout << "-------------TIME DIFF 2: " << dur.toSec() << std::endl;
             vehicle_input.vehicle_control_signals.u = T_1_U;
             vehicle_input.vehicle_control_signals.w = T_1_W;
-        } else if (dur.toSec() > T_2  && dur.toSec() < T_3) {
+        } else if (dur.toSec() < T_3) {
+            // std::cout << "-------------TIME DIFF 3: " << dur.toSec() << std::endl;
             vehicle_input.vehicle_control_signals.u = T_2_U;
             vehicle_input.vehicle_control_signals.w = T_2_W;
-        } else if (dur.toSec() > T_3  && dur.toSec() < T_4) {
+        } else if (dur.toSec() < T_4) {
+            // std::cout << "-------------TIME DIFF 4: " << dur.toSec() << std::endl;
             vehicle_input.vehicle_control_signals.u = T_3_U;
             vehicle_input.vehicle_control_signals.w = T_3_W;
-        } else if (dur.toSec() > T_4 && dur.toSec() < T_5) {
+        } else if (dur.toSec() < T_5) {
+            // std::cout << "-------------TIME DIFF 5: " << dur.toSec() << std::endl;
             vehicle_input.vehicle_control_signals.u = T_4_U;
             vehicle_input.vehicle_control_signals.w = T_4_W;
-        } else if (dur.toSec() > T_5 && dur.toSec() < T_6) {
+        } else if (dur.toSec() < T_6) {
+            // std::cout << "-------------TIME DIFF 6: " << dur.toSec() << std::endl;
             vehicle_input.vehicle_control_signals.u = T_5_U;
             vehicle_input.vehicle_control_signals.w = T_5_W;
+        } else if (dur.toSec() < T_7) {
+            // std::cout << "-------------TIME DIFF 7: " << dur.toSec() << std::endl;
+            vehicle_input.vehicle_control_signals.u = T_6_U;
+            vehicle_input.vehicle_control_signals.w = T_6_W;
         } else {
+            // std::cout << "-------------TIME DIFF 8: " << dur.toSec() << std::endl;
+            // std::cout << "-------------T_1: " << T_1 << std::endl;
             vehicle_input.vehicle_control_signals.u = FINAL_U;
             vehicle_input.vehicle_control_signals.w = FINAL_W;
         }
