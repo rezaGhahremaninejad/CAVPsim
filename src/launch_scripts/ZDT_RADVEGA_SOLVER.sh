a506ce512b85e7f1f898129d9e8786cd4c6bae15
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 <rghahremani@stu.okan.edu.tr>
2022-12-06 22:03:29 +0300

    Simulation interface script to launch multiple CAVs easily

---
+++ src/launch_scripts/ZDT_RADVEGA_SOLVER.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+echo "Set number of CAVs: "
+read CAV_NUMBER
+rosparam set use_sim_time true & 
+roslaunch communication_model default.launch &
+roslaunch ../launch_files/common.launch &
+for i in $(seq 1 $CAV_NUMBER)
+do
+  # echo "Launch CAV $i"
+  # sleep 0.1
+  roslaunch ../launch_files/ZDT_RADVEGA_SINGLE.launch vehicle_namespace:="v_$i" &
+done
